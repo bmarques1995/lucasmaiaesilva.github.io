5f2dc78a3e2eb222a6bf753b8daee2c88fd1f84a
Lucas Maia e Silva <lucasmaiaesilva@gmail.com>
2015-10-18 23:25:06 -0200

    returning cache to imagemin task

--- gulpfile.js
+++ gulpfile.js
@@ -75,7 +75,7 @@ gulp.task('js', function(){
 gulp.task('imagemin', function() {
 	return gulp.src('src/img/**/*.{jpg,png,gif}')
 		.pipe(plumber())
-		.pipe(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true }))
+		.pipe(cache(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true })))
 		.pipe(gulp.dest('assets/img/'));
 });
 
