69852082ebb8dd61da99505488e58ec3dbb7a143
lucasmaiaesilva <lucasmaiaesilva@gmail.com>
2017-12-29 18:54:49 -0200

    feat: Add images on the footer of social icons

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,11 +1,40 @@
 <template>
   <footer class="layout-footer">
-    footer
+    <div class="container">
+      <div class="footer-content">
+        <span class="icons">
+          <a href="https://github.com/lucasmaiaesilva">
+            <img class="icon" src="~/assets/img/github.png" />
+          </a>
+          <a href="https://br.linkedin.com/in/lucasmaiaesilva">
+            <img class="icon" src="~/assets/img/linkedin.png" />
+          </a>
+          <a href="https://twitter.com/lucasmaiaesilva">
+            <img class="icon" src="~/assets/img/twitter.png" />
+          </a>
+        </span>
+      </div>
+    </div>
   </footer>
 </template>
 
 <style>
 .layout-footer {
   background: #756c83;
+}
+
+.footer-content {
+  height: 3.4rem;
+}
+
+.icons {
+  height: 100%;
+  display: flex;
+  align-items: center;
+}
+
+.icon {
+  height: 2.4rem;
+  margin-right: 1rem;
 }
 </style>
