986a675d1fc9fadb746766f623e10d1a3d5c0f5d
lucasmaiaesilva <lucasmaiaesilva@gmail.com>
2017-12-29 18:15:34 -0200

    feat: Add the logo

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,11 +1,22 @@
 <template>
-  <span class="logo-text">
-    <nuxt-link to="/">lucas maia e silva</nuxt-link>
+  <span class="logo">
+    <img class="logo-image" src="~/assets/img/eu.png" />
+    <nuxt-link to="/">lucas maia</nuxt-link>
   </span>
 </template>
 
 <style>
-  .logo-text a {
+  .logo {
+    display: flex;
+    align-items: center;
+  }
+  .logo-image {
+    max-height: 3rem;
+  }
+  .logo a {
     text-decoration: none;
+    margin-left: 1rem;
+    font-family: Nunito,Helvetica Neue,Helvetica,Arial,sans-serif;
+    text-transform: capitalize;
   }
 </style>
