e012dbe3d4b02c3d7e7c33312567179cd3adf351
lucasmaiaesilva <lucasmaiaesilva@gmail.com>
2018-01-13 18:06:56 -0200

    Feat: Add images on hero

--- components/Hero.vue
+++ components/Hero.vue
@@ -10,13 +10,24 @@
 
 <style>
   .hero {
-    background: #f38181;
+    background: url("~/assets/img/background.png");
+    background-size: cover;
   }
 
   .hero-content {
-    background: #b9e1dc;
+    background: #fff url("~/assets/img/browser.png") no-repeat 50% 0;
+    padding-top: 3.5rem;
+    padding-left: 1rem;
+    background-size: 100%;
+    color: #d3d3d3;
     margin: 3rem auto;
-    height: 9rem;
+    border-radius: 8px;
+    height: 19rem;
     width: 90%;
+  }
+  @media screen and (min-width: 734px) {
+    .hero-content {
+      padding-top: 5rem;  
+    }
   }
 </style>
--- components/Logo.vue
+++ components/Logo.vue
@@ -1,6 +1,6 @@
 <template>
   <span class="logo">
-    <img class="logo-image" src="~/assets/img/eu.png" />
+    <img class="logo-image" src="~/assets/img/me.svg" />
     <nuxt-link to="/">lucas maia</nuxt-link>
   </span>
 </template>
@@ -11,9 +11,13 @@
     align-items: center;
   }
   .logo-image {
-    max-height: 3rem;
+    height: 3vw;
+    width: 3vw;
+    background: rgb(136, 0, 0);
+    border-radius: 50%;
   }
   .logo a {
+    width: 10rem;
     text-decoration: none;
     margin-left: 1rem;
     font-family: Nunito,Helvetica Neue,Helvetica,Arial,sans-serif;
